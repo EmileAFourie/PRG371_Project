043f426d8e4a0aa612e3cd7c7e5b9770d7623e0c
Cameron <86959567+CameronJWood@users.noreply.github.com>
2023-08-27 22:18:15 +0200

    2222

--- Java_SpringBoot_WebApp/src/main/java/com/PRG371/Java_SpringBoot_WebApp/student/StudentConfig.java
+++ Java_SpringBoot_WebApp/src/main/java/com/PRG371/Java_SpringBoot_WebApp/student/StudentConfig.java
@@ -1,38 +1,4 @@
-//package com.PRG371.Java_SpringBoot_WebApp.student;
-//
-//import java.util.*;
-//
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class StudentConfig {
-//    @Bean
-//    CommandLineRunner cLR(StudentRepository repo)
-//    {
-//        return args ->
-//        {
-//            Student lyle = new Student(
-//                    "Lyle",
-//                    "4 Puk Street",
-//                    "lyle@gmail.com",
-//                    "curryPowder"
-//            );
-//
-//            Student mmarona = new Student(
-//                    "Mmarona",
-//                    "10 Key Street",
-//                    "mmarona@gmail.com",
-//                    "girlPower"
-//            );
-//
-//            repo.saveAll(
-//                    List.of(lyle,mmarona)
-//            );
-//        };
-//    }
-//}
+
 
 package com.PRG371.Java_SpringBoot_WebApp.student;
 
@@ -65,21 +31,21 @@ public class StudentConfig {
                 repo.save(mmarona);
             }
 
-            if (repo.findStudentByEmail("wood@gmail.com").isEmpty()) { // Fixed email check
+            if (repo.findStudentByEmail("wood@gmail.com").isEmpty()) {
                 Student cameron = new Student(
                         "Cameron",
                         "10 Bruh Street",
-                        "wood@gmail.com", // Fixed email
+                        "wood@gmail.com",
                         "ManPower"
                 );
                 repo.save(cameron);
             }
 
-            if (repo.findStudentByEmail("chaaadddd@gmail.com").isEmpty()) { // Fixed email check
+            if (repo.findStudentByEmail("chaaadddd@gmail.com").isEmpty()) {
                 Student chaaaadddddd = new Student(
                         "chaaad",
                         "10 tablemountain Street",
-                        "table@gmail.com", // Fixed email
+                        "chaaadddd@gmail.com",
                         "tablePower"
                 );
                 repo.save(chaaaadddddd);
